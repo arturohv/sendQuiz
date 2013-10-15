d9f5b304d3c3ab8fae5754c604fce7c7a0b7133f
arturohv <arturohv@gmail.com>
2013-10-15 13:47:57 -0700

    Ya envia correos

--- PHPMailer/gmail.php
+++ PHPMailer/gmail.php
@@ -16,15 +16,13 @@ $mail->isSMTP();
 // 2 = client and server messages
 $mail->SMTPDebug = 0;
 //Ask for HTML-friendly debug output
-
-$mail->SMTPSecure = 'tls';
 $mail->Debugoutput = 'html';
 //Set the hostname of the mail server
 $mail->Host = 'smtp.gmail.com';
 //Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
 $mail->Port = 587;
 //Set the encryption system to use - ssl (deprecated) or tls
-//$mail->SMTPSecure = 'tls';
+$mail->SMTPSecure = 'tls';
 //Whether to use SMTP authentication
 $mail->SMTPAuth = true;
 //Username to use for SMTP authentication - use full email address for gmail
